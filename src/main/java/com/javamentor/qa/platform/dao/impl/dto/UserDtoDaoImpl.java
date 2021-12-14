72dc4ae285e306081d33cd82ec52548368ade3ab
rashi <rashid058@mial.ru>
2021-12-14 17:24:21 +0500

    14.12.2021

--- src/main/java/com/javamentor/qa/platform/dao/impl/dto/UserDtoDaoImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/dto/UserDtoDaoImpl.java
@@ -16,7 +16,8 @@ public class UserDtoDaoImpl implements UserDtoDao {
     @Override
     public List<UserDto> findUserDto(Long id) {
         return entityManager.createQuery("SELECT NEW com.javamentor.qa.platform.models.dto.UserDto(" +
-                        "e.id, e.email, e.fullName, 'нет фото', e.city, 1) FROM User e where e.id ="+id)
+                        "e.id, e.email, e.fullName, e.imageLink, e.city, 1) FROM User e where e.id=:id")
+                .setParameter("id", id)
                 .getResultList();
     }
 }
